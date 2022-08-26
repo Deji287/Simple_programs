149e6e26b22034014522130d6eff532866882f73
Deji287 <slimdejavu0101@gmail.com>
2022-08-26 06:44:12 +0100

    Code to find the longest word in a sentence

--- find_longest_word.js
+++ find_longest_word.js
@@ -2,9 +2,11 @@
 
 const findTheLongestWord = (str) => {
   const words = str.split(" ");
+  let presentLongestWord = "";
   words.forEach((word) => {
-    console.log(word);
+    if (word.length > presentLongestWord.length) presentLongestWord = word;
   });
+  return presentLongestWord;
 };
 console.log(
   findTheLongestWord("The longest word in this sentence is assignment")
